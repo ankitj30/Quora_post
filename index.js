f4368f003d5983e9d5e1c1bb96526e50b8b4d030
Ankit Jaiswal <ankitjaiswal3016@gmail.com>
2026-01-05 01:56:40 +0530

    Make app Render compatible (PORT fix)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const methodOverride = require('method-override');
+
+// ✅ Render-compatible PORT
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -19,21 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Sample posts
 let posts = [
     { id: uuidv4(), username: 'hardik_pandya', title: 'i love coding !!' },
-    { id: uuidv4(), username: 'Ankit.jaiswal', title: 'work hard  !!' },
+    { id: uuidv4(), username: 'Ankit.jaiswal', title: 'work hard !!' },
     { id: uuidv4(), username: 'nirajxxk', title: 'heyy buddy !!' },
     { id: uuidv4(), username: 'its.ishwar', title: 'i got selected for my first internship !!' },
     { id: uuidv4(), username: 'yoursSona', title: 'be positive !!' },
     { id: uuidv4(), username: 'itsRam12.jaxx', title: 'heyy buddy !!' }
 ];
 
-// Show all posts
-app.get('/posts', (req, res) => {
-    res.render('index.ejs', { posts: posts });
-});
-
 // Redirect root to /posts
 app.get('/', (req, res) => {
     res.redirect('/posts');
+});
+
+// Show all posts
+app.get('/posts', (req, res) => {
+    res.render('index.ejs', { posts });
 });
 
 // Show new post form
@@ -41,12 +43,12 @@ app.get('/posts/new', (req, res) => {
     res.render('new.ejs');
 });
 
-// Create new post FIXED (added id)
+// Create new post
 app.post('/posts', (req, res) => {
-    let { username, title } = req.body;
+    const { username, title } = req.body;
 
     posts.push({
-        id: uuidv4(),  // <-- IMPORTANT FIX
+        id: uuidv4(),
         username,
         title
     });
@@ -56,35 +58,43 @@ app.post('/posts', (req, res) => {
 
 // Show post details
 app.get('/posts/:id', (req, res) => {
-    let { id } = req.params;
-    let post = posts.find(p => p.id === id);
+    const { id } = req.params;
+    const post = posts.find(p => p.id === id);
 
     if (!post) {
         return res.send("Post not found!");
     }
 
-    res.render('show.ejs', { post: post });
+    res.render('show.ejs', { post });
 });
 
+// Edit form
+app.get('/posts/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const post = posts.find(p => p.id === id);
 
-app.patch('/posts/:id', (req, res) => {
-    let { id } = req.params;
-    let newtitle = req.body.title;
-    let post = posts.find(p => p.id === id);
     if (!post) {
         return res.send("Post not found!");
     }
-    post.title = newtitle;
+
+    res.render('edit.ejs', { post });
+});
+
+// Update post
+app.patch('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const newTitle = req.body.title;
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        return res.send("Post not found!");
+    }
+
+    post.title = newTitle;
     res.redirect(`/posts/${id}`);
 });
 
-app.get('/posts/:id/edit', (req, res) => {
-    let { id } = req.params;
-    let post = posts.find(p => p.id === id);
-    res.render('edit.ejs' , { post: post });
-});
-
 // Start server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
